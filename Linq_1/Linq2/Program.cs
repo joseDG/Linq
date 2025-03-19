8df1e73894800a9791542b4de0cdc29535f4d627
joseDG <josethp1@gmail.com>
2025-03-18 23:02:03 -0500

    Linq parte 2

--- Linq_1/Linq2/Program.cs
+++ Linq_1/Linq2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+
         /*List<Employee> employeeList = Data.GetEmployees();
 
         var filteredEmployees = employeeList.Where(e => e.IsManager == true);
@@ -18,7 +18,7 @@ class Program
             Console.WriteLine();
         }
 
-        Console.ReadKey();*/
+        Console.ReadKey();
 
         List<Department> departmentList = Data.GetDepartments();
 
@@ -30,8 +30,44 @@ class Program
             Console.WriteLine($"Short Name: {department.ShortName}");
             Console.WriteLine($"Long Name: {department.LongName}");
             Console.WriteLine();
+        }*/
+
+        List<Employee> employeeList = Data.GetEmployees();
+        List<Department> departmentList = Data.GetDepartments();
+
+        var resultList = from emp in employeeList
+                         join dept in departmentList
+                         on emp.DepartmentId equals dept.Id
+                         select new
+                         {
+                             FirstName = emp.FirstName,
+                             LastName = emp.LastName,
+                             AnnualSalary = emp.AnnualSalary,
+                             Manager = emp.IsManager,
+                             Department = dept.LongName,
+                         };
+
+        foreach (var employee in resultList)
+        {
+            Console.WriteLine($"First Name: {employee.FirstName}");
+            Console.WriteLine($"Last Name: {employee.LastName}");
+            Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+            Console.WriteLine($"Manager: {employee.Manager}");
+            Console.WriteLine($"Department: {employee.Department}");
+            Console.WriteLine();
         }
 
+        var averageAnnualSalary = resultList.Average(a => a.AnnualSalary);
+        var highestAnnualSalary = resultList.Max(a => a.AnnualSalary);
+        var lowewtAnnualSalary = resultList.Min(a => a.AnnualSalary);
+
+        Console.WriteLine($"Average Anual Salary: {averageAnnualSalary}");
+        Console.WriteLine($"Highest Anual Salary: {highestAnnualSalary}");
+        Console.WriteLine($"Lowest Annual Salary: {lowewtAnnualSalary}");
+
+
         Console.ReadKey();
+
+
     }
 }
---
+++ Linq_1/Linq3/Program.cs
@@ -0,0 +1,102 @@
+﻿using Linq3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Employee> employeesList = Data.GetEmployees();
+        List<Department> departmentsList = Data.GetDepartments();
+
+        //Select and Where Operators - Method Syntaxis
+        //var results = employeesList.Select(e => new
+        //{
+        //    FullName = e.FirstName + "" + e.LastName,
+        //    AnnualSalary = e.AnnualSalary,
+        //}).Where(e => e.AnnualSalary >= 50000);
+
+        //foreach(var item in results)
+        //{
+        //    Console.WriteLine($"{item.FullName, -20} {item.AnnualSalary, 10}");
+        //}
+
+        //Select and Where Operators - Query Syntaxis
+        //var results = from emp in employeesList
+        //              where emp.AnnualSalary >= 50000
+        //              select new
+        //              {
+        //                  FullName = emp.FirstName + " " + emp.LastName,
+        //                  AnnualSalary = emp.AnnualSalary
+        //              };
+
+        //foreach (var item in results)
+        //{
+        //   Console.WriteLine($"{item.FullName, -20} {item.AnnualSalary, 10}");
+        //}
+
+        //Deferred(Carga Perezosa) Execution Example
+        //var results = from emp in employeesList.GetHighSalariedEmployees()
+        //              select new
+        //              {
+        //                  FullName = emp.FirstName + " " + emp.LastName,
+        //                  AnnualSalary = emp.AnnualSalary
+        //              };
+
+        //employeesList.Add(new Employee
+        //{
+        //    Id = 5,
+        //    FirstName = "Sam",
+        //    LastName = "Davis",
+        //    AnnualSalary = 100000.20m,
+        //    IsManager = true,
+        //    DepartmentId = 2
+
+        //});
+
+        //foreach (var item in results)
+        //{
+        //    Console.WriteLine($"{item.FullName,-20} {item.AnnualSalary,10}");
+        //}
+
+        ////Immediate Execution Example
+        //var results = (from emp in employeesList.GetHighSalariedEmployees()
+        //               select new
+        //               {
+        //                   FullName = emp.FirstName + " " + emp.LastName,
+        //                   AnnualSalary = emp.AnnualSalary
+        //               }).ToList();
+
+        //employeesList.Add(new Employee
+        //{
+        //    Id = 5,
+        //    FirstName = "Sam",
+        //    LastName = "Davis",
+        //    AnnualSalary = 100000.20m,
+        //    IsManager = true,
+        //    DepartmentId = 2
+
+        //});
+
+        //foreach (var item in results)
+        //{
+        //    Console.WriteLine($"{item.FullName,-20} {item.AnnualSalary,10}");
+        //}
+
+        Console.ReadKey();
+    }
+
+}
+
+public static class EnumerableCollectionExtensionMethods
+{
+    public static IEnumerable<Employee> GetHighSalariedEmployees(this IEnumerable<Employee> employees)
+    {
+        foreach (Employee emp in employees)
+        {
+
+            Console.WriteLine($"Accessing employee: {emp.FirstName + " " + emp.LastName}");
+
+            if (emp.AnnualSalary >= 50000)
+                yield return emp;
+        }
+    }
+}
